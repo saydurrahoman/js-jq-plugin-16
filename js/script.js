622e46189a10919a1312a9d11df35eac6479cd8a
md saydur rahoman <145344840+saydurrahoman@users.noreply.github.com>
2024-01-04 04:07:57 -1200

    Add files via upload

---
+++ js/script.js
@@ -0,0 +1,15 @@
+// addClass
+$(document).ready(function(){
+  $('.center-slider').slick({
+    slidesToShow: 5,
+    slidesToScroll: 1,
+    centerMode: true,
+    arrows: true,
+    dots: false,
+    speed: 300,
+    centerPadding: '20px',
+    infinite: true,
+    autoplaySpeed: 5000,
+    autoplay: true
+  });
+})
